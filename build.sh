fd91b6bb08f0387e8a300ff2e8f0977e075e9533
Auston Stewart <auston@shedbuilt.net>
2018-01-12 02:20:26 -0500

    Adding the toolchain build mode

--- build.sh
+++ build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-FORCE_UNSAFE_CONFIGURE=1  \
-./configure --prefix=/usr \
-            --bindir=/bin || return 1
+case "$SHED_BUILDMODE" in
+    toolchain)
+        ./configure --prefix=/tools || return 1
+        ;;
+    *)
+        FORCE_UNSAFE_CONFIGURE=1  \
+        ./configure --prefix=/usr \
+                    --bindir=/bin || return 1
+        ;;
+esac
 make -j $SHED_NUMJOBS || return 1
 make DESTDIR="$SHED_FAKEROOT" install || return 1
-make -C doc DESTDIR="$SHED_FAKEROOT" install-html docdir=/usr/share/doc/tar-1.30 || return 1
+
+if [ "$SHED_BUILDMODE" != 'toolchain' ]; then
+    make -C doc DESTDIR="$SHED_FAKEROOT" install-html docdir=/usr/share/doc/tar-1.30 || return 1
+fi
