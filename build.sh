41ebc380a05e04a4ab0af6dcfb9bef4f1a337754
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:28:54 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-case "$SHED_BUILDMODE" in
+case "$SHED_BUILD_MODE" in
     toolchain)
         FORCE_UNSAFE_CONFIGURE=1 \
         ./configure --prefix=/tools || return 1
@@ -10,9 +10,9 @@ case "$SHED_BUILDMODE" in
                     --bindir=/bin || return 1
         ;;
 esac
-make -j $SHED_NUMJOBS || return 1
-make DESTDIR="$SHED_FAKEROOT" install || return 1
+make -j $SHED_NUM_JOBS || return 1
+make DESTDIR="$SHED_FAKE_ROOT" install || return 1
 
-if [ "$SHED_BUILDMODE" != 'toolchain' ]; then
-    make -C doc DESTDIR="$SHED_FAKEROOT" install-html docdir=/usr/share/doc/tar-1.30 || return 1
+if [ "$SHED_BUILD_MODE" != 'toolchain' ]; then
+    make -C doc DESTDIR="$SHED_FAKE_ROOT" install-html docdir=/usr/share/doc/tar-1.30 || return 1
 fi
